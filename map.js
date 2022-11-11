e9a2bf8f8b1d6ab4c7bf7ae847e6142a3061a6c7
bryanparmelee <bryan.parmelee@gmail.com>
2022-11-10 23:15:55 -0500

    Update gitignore

--- map.js
+++ map.js
@@ -21,13 +21,3 @@ function closeFull() {
   fullBox.style.display = "none";
 }
 
-// let lastScrollTop = 0;
-// window.onscroll = () => {
-//   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//   if (scrollTop > lastScrollTop || scrollTop < 0 ) {
-//     navbar.style.top = "-80px";
-//   } else {
-//     navbar.style.top = "0";
-//   }
-//   lastScrollTop = scrollTop;
-// };
