99bbf00d839cc07fbeeb056465c926ba75d349e0
bryanparmelee <bryan.parmelee@gmail.com>
2022-11-10 22:42:21 -0500

    Updated event info

--- map.js
+++ map.js
@@ -1,29 +1,25 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
 const navbar = document.getElementById("navbar");
 
-
-navBarToggle.addEventListener('click', function () {
-    
-    mainNav.classList.toggle('active');
+navBarToggle.addEventListener("click", function () {
+  mainNav.classList.toggle("active");
 });
 
-const fullBox = document.getElementById('full-box');
-fullBox.addEventListener('click', closeFull());
+const fullBox = document.getElementById("full-box");
+fullBox.addEventListener("click", closeFull());
 
-const full = document.getElementById('full');
+const full = document.getElementById("full");
 
 function openFull(pic) {
-  let regex = /-tb/
-  full.src = pic.replace(regex, '');
+  let regex = /-tb/;
+  full.src = pic.replace(regex, "");
   fullBox.style.display = "flex";
-
-};
+}
 
 function closeFull() {
   fullBox.style.display = "none";
-};
-
+}
 
 // let lastScrollTop = 0;
 // window.onscroll = () => {
